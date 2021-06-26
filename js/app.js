e45b6b0c4825dd912d2e3d15b3306f70b3d15236
Oleg <oleg@localhost>
2021-06-26 08:59:17 +0300

    added some code for check whose commit

--- js/app.js
+++ js/app.js
@@ -7,3 +7,22 @@
         });
     }
 })();
+
+// Added some code
+//=========async&event_loop===============
+console.log('Start')
+
+console.log('Start 2')
+
+setTimeout(() => console.log('timeout 0!'), 0)
+
+window.setTimeout(() => {
+    console.log('timeout 5sec')
+}, 5000)
+
+window.setTimeout(() => {
+    console.log('timeout 2sec')
+}, 2000)
+
+console.log('end')
+//=====================
